2a1d6684755ec23894d9040849582dfc12e64697
Deby Utari <debydexel@gmail.com>
2023-01-13 20:13:26 +0700

    initial messages

---
+++ src/main/java/dujavafundamental/tesharian/TesTemplate.java
@@ -0,0 +1,15 @@
+package dujavafundamental.tesharian;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 13/01/2023 20.10
+@Last Modified 13/01/2023 20.10
+Version 1.0
+*/
+
+public class TesTemplate {
+}
+
+
+   
