d9eb0120ce87dd51d28c05f05620fdd0aafdd701
Deby Utari <debydexel@gmail.com>
2023-01-13 18:13:33 +0700

    initial messages

---
+++ TestTemplateDU.java
@@ -0,0 +1,13 @@
+public class TestTemplateDU {
+}
+/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 13/01/2023 18.03
+@Last Modified 13/01/2023 18.03
+Version 1.0
+*/
+
+   
