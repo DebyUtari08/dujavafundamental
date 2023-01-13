dff2d15816e5edf5163206158dc4c1ed935631de
Deby Utari <debydexel@gmail.com>
2023-01-13 19:43:35 +0700

    initial messages

--- src/main/java/dujavafundamental/TesTemplate.java
+++ src/main/java/dujavafundamental/TesTemplate.java
@@ -1,13 +1,15 @@
-package dujavafundamental;
-
-public enum TestTemplateEnum {
-}
-/*
+package dujavafundamental;/*
 IntelliJ IDEA 2022.2.3 (Ultimate Edition)
 Build #IU-222.4345.14, built on October 5, 2022
 @Author LENOVO a.k.a. Deby Utari
 Java Developer
-Created on 13/01/2023 18.53
-@Last Modified 13/01/2023 18.53
+Created on 13/01/2023 19.42
+@Last Modified 13/01/2023 19.42
 Version 1.0
 */
+
+public class TesTemplate {
+}
+
+
+   
