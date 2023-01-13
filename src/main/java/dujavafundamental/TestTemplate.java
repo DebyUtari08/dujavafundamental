5fc24e2244b93db78c3f9a75093e2afa242e1c6f
Deby Utari <debydexel@gmail.com>
2023-01-13 18:49:34 +0700

    initial messages

--- src/main/java/dujavafundamental/TestTemplate.java
+++ src/main/java/dujavafundamental/TestTemplate.java
@@ -7,8 +7,8 @@ IntelliJ IDEA 2022.2.3 (Ultimate Edition)
 Build #IU-222.4345.14, built on October 5, 2022
 @Author LENOVO a.k.a. Deby Utari
 Java Developer
-Created on 13/01/2023 18.41
-@Last Modified 13/01/2023 18.41
+Created on 13/01/2023 18.46
+@Last Modified 13/01/2023 18.46
 Version 1.0
 */
 
