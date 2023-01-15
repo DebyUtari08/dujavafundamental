626bf7cfa93b83f004f2178bc97dcd17e281ed30
Deby Utari <debydexel@gmail.com>
2023-01-15 13:43:40 +0700

    initial messages

--- src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
@@ -1,12 +1,11 @@
 package com.juaracoding.dujavafundamental.tesharian.testarray;
-
 /*
 IntelliJ IDEA 2022.2.3 (Ultimate Edition)
 Build #IU-222.4345.14, built on October 5, 2022
 @Author LENOVO a.k.a. Deby Utari
 Java Developer
-Created on 15/01/2023 13.29
-@Last Modified 15/01/2023 13.29
+Created on 15/01/2023 13.43
+@Last Modified 15/01/2023 13.43
 Version 1.0
 */
 
