75bd318e16d56adf47ab3424b78d18fccae199a6
Deby Utari <debydexel@gmail.com>
2023-01-16 00:34:05 +0700

    initial messages

--- src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
@@ -10,7 +10,29 @@ Version 1.0
 */
 
 public class TestArray {
-}
+
+        public static void main(String[] args) {
+
+            int[] intArr = {3,8,5,2,1,9,10};
+            int intB = 1;
+            boolean isFound = true;
+
+            for(int i=0;i<intArr.length;i++)
+            {
+                if(intB==intArr[i])
+                {
+                    System.out.println("Angka "+intB+" sudah ditemukan , pada index ke "+i);
+                    isFound = false;
+                    break;
+                }
+            }
+
+            if(isFound)
+            {
+                System.out.println("Angka  "+intB+" yang anda cari tidak ditemukan !! ");
+            }
+        }
+    }
 
 
    
