9913073597ec64ed392f8b6da5b31f081afb9a0e
Deby Utari <debydexel@gmail.com>
2023-01-15 14:19:44 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_1/Praktikum1.java
@@ -0,0 +1,15 @@
+package com.juaracoding.dujavafundamental.tesharian.praktikum_1;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 15/01/2023 14.19
+@Last Modified 15/01/2023 14.19
+Version 1.0
+*/
+
+public class Praktikum1 {
+}
+
+
+   
