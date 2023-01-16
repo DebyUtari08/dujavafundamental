b48cb29cb73fe231992b2192fd62fe2a68fef143
Deby Utari <debydexel@gmail.com>
2023-01-16 15:46:38 +0700

    initial messages

--- src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_1/Praktikum1.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_1/Praktikum1.java
@@ -14,9 +14,8 @@ public class Praktikum1 {
     public static void main(String[] args) {
         String strInput = " ";
         Scanner sc = new Scanner(System.in);
-        System.out.println("Masukan Data :" +strInput);
+        System.out.println("Masukan Data :" + strInput);
         strInput = sc.nextLine();
-
 
 
         char chX = 'I';
@@ -24,25 +23,26 @@ public class Praktikum1 {
         System.out.println(intX);
 
         int intJumlah = 0;
-        for (int i=0; i<strInput.length(); i++) {
+        for (int i = 0; i < strInput.length(); i++) {
             chX = strInput.charAt(i);
             intX = chX;
-            if (intX >= 32 && intX <= 90)
+            {
+                if (intX >= 32 && intX <= 90)
                 //if ((intx >= 48 && intx <= 57) || (intx >= 65 && intx <= 90)
-                      //  || (intx >= 97 && <= 122)){
-                   // if ((intx >= 97 && <= 122))
-                    {
-                        intJumlah = intJumlah + intX;
-                    }
-
-                    System.out.println("INDEK KE"+i+" ADALAH HURUF "+chX+" KodeAscii "+ intX);
+                //  || (intx >= 97 && <= 122)){
+                // if ((intx >= 97 && <= 122))
+                {
+                    intJumlah = intJumlah + intX;
+                    System.out.println("INDEK KE" + i + " ADALAH HURUF " + chX + " KodeAscii " + intX);
 
                 }
+            }
 
-        System.out.println("Nilai INTX:"+intJumlah);
+        }
+
+            System.out.println("Nilai INTX:" + intJumlah);
     }
 }
-
 
 
    
