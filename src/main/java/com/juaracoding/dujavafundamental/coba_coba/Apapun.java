9ab541d26c5ee76d076e2b56d1938a91aece65a4
Deby Utari <debydexel@gmail.com>
2023-01-18 20:47:47 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/coba_coba/Apapun.java
@@ -0,0 +1,38 @@
+package com.juaracoding.dujavafundamental.coba_coba;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 18/01/2023 20.02
+@Last Modified 18/01/2023 20.02
+Version 1.0
+*/
+
+import java.util.ArrayList ;
+import java.util.List;
+
+public class Apapun //modifier
+{
+        public static void main(String[] args) {
+
+                Apapun classX = new Apapun();
+                System.out.println("PRINT OBJECT CLASS X " + classX);
+                Apapun classX1 = new Apapun();
+                System.out.println("PRINT OBJECT CLASS X1 " + classX1);
+                Apapun classX2 = new Apapun();
+                System.out.println("PRINT OBJECT CLASS X2 " + classX2);
+
+                List list = new ArrayList();
+                List list1 = new ArrayList();
+                List list2 = new ArrayList();
+
+//        classX.hitungLuasSegitiga();
+
+//        java.sql.Date d = new java.sql.Date(System.currentTimeMillis());
+//        Date da = new Date();
+
+        }
+}
+
+
+   
