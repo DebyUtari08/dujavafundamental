b2a1cb7187a99fea4e67489f03b8a8ca6c73577d
Deby Utari <debydexel@gmail.com>
2023-01-18 21:10:16 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/coba_coba/TestThis.java
@@ -0,0 +1,18 @@
+package com.juaracoding.dujavafundamental.coba_coba;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 18/01/2023 21.04
+@Last Modified 18/01/2023 21.04
+Version 1.0
+*/
+
+public class TestThis {
+     private TestThis () {
+
+    }
+}
+
+
+   
