dd0042258f72f5d8a0151fe2e90816b5a32550f8
Deby Utari <debydexel@gmail.com>
2023-01-20 13:37:45 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_3/Praktikum3.java
@@ -0,0 +1,18 @@
+package com.juaracoding.dujavafundamental.tesharian.praktikum_3;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 20/01/2023 13.36
+@Last Modified 20/01/2023 13.36
+Version 1.0
+*/
+
+public class Praktikum3 {
+  /*
+	Soal Nomor 1 Praktikum 3 Java Fundamental
+*/
+}
+
+
+   
