84a359c79b9dc9ce3d91c08733cd0477a19b590d
Deby Utari <debydexel@gmail.com>
2023-01-20 13:10:27 +0700

    initial messages

--- src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_2/Praktikum2.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_2/Praktikum2.java
@@ -7,22 +7,43 @@ Created on 19/01/2023 12.50
 @Last Modified 19/01/2023 12.50
 Version 1.0
 */
-import  java.lang.*;
+
+//import  java.lang.*;
+import java.util.Scanner;
 
 public class Praktikum2 {
     /*
-	Soal Nomor 2 Praktikum 2 Java Fundamental
+	Soal Nomor 1 Praktikum 2 Java Fundamental
     */
     public static void main(String[] args) {
-        String strNamadepan = "";
-        System.out.println("Input Nama Depan" +strNamadepan);
-        String strNamabelakang = "";
-        System.out.println("Input Nama Belakang" +strNamabelakang);
+
+        //deklarasi dan inisialisasi
+        //deklarasi dan inisialisasi
+        Scanner sc = new Scanner(System.in);
+        int intNumber = 0;
+        float rata, total= 0.0F;
+        int i= 0;
+        boolean isAgain = true;
 
 
+        try {
+            System.out.println("Masukkan angka sepuasnya: ");
 
+
+            do {
+                intNumber = sc.nextInt();//input
+                total += intNumber;//proses penjumlahan input
+                i++;
+
+            } while (isAgain); //kondisi
+
+
+        } catch (Exception e) {//exception handling utk input karakter
+
+            rata = total/i;
+            System.out.println("Jumlah : " + total);//output
+            System.out.println("Rata-rata yang dihasilkan :" +rata);
+            System.exit(0);//keluar dari sistem
+        }
     }
 }
-
-
-   
--- src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/testarray/TestArray.java
@@ -9,30 +9,32 @@ Created on 15/01/2023 13.43
 Version 1.0
 */
 
+import java.util.Scanner;
+
 public class TestArray {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int[] intArr = {3,8,5,2,1,9,10};
-            int intB = 1;
-            boolean isFound = true;
+        /*int[] intArr = {3, 8, 5, 2, 1, 9, 10};
+        int intB = 1;
+        boolean isFound = true;
 
-            for(int i=0;i<intArr.length;i++)
-            {
-                if(intB==intArr[i])
-                {
-                    System.out.println("Angka "+intB+" sudah ditemukan , pada index ke "+i);
-                    isFound = false;
-                    break;
-                }
-            }
-
-            if(isFound)
-            {
-                System.out.println("Angka  "+intB+" yang anda cari tidak ditemukan !! ");
+        for (int i = 0; i < intArr.length; i++) {
+            if (intB == intArr[i]) {
+                System.out.println("Angka " + intB + " sudah ditemukan , pada index ke " + i);
+                isFound = false;
+                break;
             }
         }
+
+        if (isFound) {
+            System.out.println("Angka  " + intB + " yang anda cari tidak ditemukan !! ");
+        }
     }
+}/*
 
 
-   
+         */
+
+   }
+}
