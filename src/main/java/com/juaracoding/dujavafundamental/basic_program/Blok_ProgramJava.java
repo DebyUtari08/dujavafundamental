f7951b937b4012db07e4c42a80233f7d425eb2ad
Deby Utari <debydexel@gmail.com>
2023-01-23 16:05:40 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/basic_program/Blok_ProgramJava.java
@@ -0,0 +1,29 @@
+package com.juaracoding.dujavafundamental.basic_program;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 23/01/2023 15.56
+@Last Modified 23/01/2023 15.56
+Version 1.0
+*/
+
+public class Blok_ProgramJava {
+    public static void main (String [] args){
+        System.out.println("Hello World");
+        System.out.println("Hello Kode");
+
+        //blok program if
+        if (true){
+            System.out.println("True");
+        }
+
+        //blok program for
+        for (int i=0; i<10; i++){
+            System.out.println("Perulangan ke"+i+"");
+        }
+    }
+}
+
+
+   
