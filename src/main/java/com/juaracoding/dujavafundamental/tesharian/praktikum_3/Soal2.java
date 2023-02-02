b19ae88bb83866f7adfcd60340b81d82ca523d59
Deby Utari <debydexel@gmail.com>
2023-02-02 12:43:44 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_3/Soal2.java
@@ -0,0 +1,15 @@
+package com.juaracoding.dujavafundamental.tesharian.praktikum_3;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 24/01/2023 19.08
+@Last Modified 24/01/2023 19.08
+Version 1.0
+*/
+
+public class Soal2 {
+}
+
+
+   
