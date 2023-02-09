34a363901907af63287129399b329746dd71bfa5
Deby Utari <debydexel@gmail.com>
2023-02-09 17:14:56 +0700

    initial messages

--- src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_6/Praktikum6.java
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_6/Praktikum6.java
@@ -15,37 +15,35 @@ public class Praktikum6 {
 	Soal Nomor 1 Praktikum 6 Java Fundamental
     */
     public static void main(String[] args) {
+        String lagi = "Y";
+        Scanner yesno = new Scanner(System.in);
+        while (lagi.equals("Y")) {
+            ArrayList<Integer> nilai = new ArrayList();
+            Scanner Input = new Scanner(System.in);
+            System.out.print("Input Panjang Array:");
+            int panjang = Input.nextInt();
+            for (int i = 0; i < panjang; i++) {
+                System.out.print("Index ke " + i + ":");
+                int angka = Input.nextInt();
+                nilai.add(angka);
+            }
 
-        ArrayList<Integer> nilai = new ArrayList();
-        Scanner Input = new Scanner(System.in);
-
-        System.out.print("Input Panjang Array:");
-        int panjang = Input.nextInt();
-        for (int i = 0; i < panjang; i++) {
-            System.out.print("Index ke " + i + ":");
-            int angka = Input.nextInt();
-            nilai.add(angka);
-        }
-
-
-        System.out.println("Inputan Bilangan Bulat Sebelum di Sorting :" + nilai + "");
-        for (int i = 0; i < nilai.size(); i++) {
-            for (int j = 0; j < nilai.size() - 1; j++) {
-                if (nilai.get(j) > nilai.get(j + 1)) {
-                    int temp = nilai.get(j);
-                    nilai.set(j, nilai.get(j + 1));
-                    nilai.set(j + 1, temp);
+            System.out.println("Inputan Bilangan Bulat Sebelum di Sorting :" + nilai + "");
+            for (int i = 0; i < nilai.size(); i++) {
+                for (int j = 0; j < nilai.size() - 1; j++) {
+                    if (nilai.get(j) > nilai.get(j + 1)) {
+                        int temp = nilai.get(j);
+                        nilai.set(j, nilai.get(j + 1));
+                        nilai.set(j + 1, temp);
+                    }
                 }
             }
+            System.out.println("Setelah di Sorting :" + nilai + "");
+            System.out.println("Apakah Ingin Menyorting lagi? (Y/N)");
+            lagi = yesno.nextLine().toUpperCase();
         }
-        String lagi= "Y";
-        while (lagi.equals("Y"));
-        Scanner yesno = new Scanner(System.in);
-        System.out.println("Setelah di Sorting :" + nilai + "");
-        System.out.println("Apakah Ingin Menyorting lagi? (Y/N)");lagi = yesno.nextLine().toUpperCase();
     }
 }
-
 
 
    
