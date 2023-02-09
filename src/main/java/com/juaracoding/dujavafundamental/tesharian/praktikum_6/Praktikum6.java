7c3d8e84daa99ffd71d0a8ae540cc34952a6168e
Deby Utari <debydexel@gmail.com>
2023-02-09 16:16:52 +0700

    initial messages

---
+++ src/main/java/com/juaracoding/dujavafundamental/tesharian/praktikum_6/Praktikum6.java
@@ -0,0 +1,33 @@
+package com.juaracoding.dujavafundamental.tesharian.praktikum_6;/*
+IntelliJ IDEA 2022.2.3 (Ultimate Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author LENOVO a.k.a. Deby Utari
+Java Developer
+Created on 09/02/2023 13.03
+@Last Modified 09/02/2023 13.03
+Version 1.0
+*/
+import java.util.*;
+import  java.lang.Math;
+
+public class Praktikum6 {
+    /*
+	Soal Nomor 1 Praktikum 6 Java Fundamental
+    */
+    public static void main ( String [] args) {
+        ArrayList<Integer> nilai= new  ArrayList();
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Input Panjang Array:");
+       int panjang = Input.nextInt();
+       for (int i=0; i<panjang; i++){
+           System.out.print("Index ke "+i+ ":");
+           int angka = Input.nextInt();
+           nilai.add(angka);
+       }
+       System.out.println("Inputan Bilangan Bulat Sebelum di Sorting :"+nilai+"");
+    }
+}
+
+
+
+   
